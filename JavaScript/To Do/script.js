ba509f7f628e65fce96b863e33f4582a914a56dd
Èrik Lledó <sonwerik@gmail.com>
2024-11-27 20:21:45 +0100

    solved errors

--- JavaScript/To Do/script.js
+++ JavaScript/To Do/script.js
@@ -1,37 +1,38 @@
-const add  = document.getElementById('add-task');
+const addTask = document.getElementById('add-task');
 const taskContainer = document.getElementById('task-container');
 const inputTask = document.getElementById('input-task');
 
-addTask.addEventListener('click', function(){
+addTask.addEventListener('click', function () {
     let task = document.createElement('div');
     task.classList.add('task');
 
     let li = document.createElement('li');
-    li.innerText= `${inputTask.value}`;
-
+    li.innerText = `${inputTask.value}`;
     task.appendChild(li);
 
+    let checkButton = document.createElement("button");
+    checkButton.innerHTML = '<i class="fa-solid fa-check"></i>';
+    checkButton.classList.add('checkTask');
+    task.appendChild(checkButton);
+
     let deleteButton = document.createElement("button");
-    deleteButton.innerHTML = '<i class="fa-solid fa-check"></i>';
+    deleteButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
     deleteButton.classList.add('deleteTask');
     task.appendChild(deleteButton);
 
-    if(inputTask.value === ""){
-        alert('Si us plau introdueix una tasca.')
+    if (inputTask.value === "") {
+        alert('Si us plau introdueix una tasca.');
     } else {
-        taskContainer.appendChild(task)
+        taskContainer.appendChild(task);
     }
 
     inputTask.value = "";
 
-    checkButton.addEventListener('click', function(){
-        checkButton.parentElement.style.textDecoration = 
-        "line-throungh";
+    checkButton.addEventListener('click', function () {
+        li.style.textDecoration = "line-through";
     });
 
-    deleteButton.addEventListener('click', function(e){
-        let target = e.target;
-        target.parentElement.parentElement.remove();
+    deleteButton.addEventListener('click', function () {
+        task.remove();
     });
-
 });
